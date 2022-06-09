cd549147946d0d052a7c9deedb266439e358cfeb
lfeldman <lukasz.feldman@oracle.com>
2022-06-09 13:34:27 +0200

    Fixed orm/provider.tf

--- orm/provider.tf
+++ orm/provider.tf
@@ -1,7 +1,7 @@
 ## Copyright (c) 2021 Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-/*terraform {
+terraform {
   required_version = ">= 1.0"
   required_providers {
     oci = {
@@ -9,7 +9,7 @@
     }
   }
 }
-*/
+
 
 provider "oci" {
   alias            = "targetregion"
